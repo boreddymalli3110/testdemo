3c55404e040872691c81190fe11f76bbb48f8c44
boreddymalli3110 <120163419+boreddymalli3110@users.noreply.github.com>
2024-04-05 10:35:31 +0530

    Add files via upload
    updated procedure file

--- procedure.sql
+++ procedure.sql
@@ -3,5 +3,5 @@ CREATE OR REPLACE PROCEDURE filter_by_role(tableName VARCHAR, role VARCHAR)
   LANGUAGE PYTHON
   RUNTIME_VERSION = '3.8'
   PACKAGES = ('snowflake-snowpark-python')
-  IMPORTS = ('@example_db.example_schema.snowflake_extensions/branches/main/python-handlers/filter.py')
+  IMPORTS = ('@insurance_db.public.git_sample/branches/main/filter.py')
   HANDLER = 'filter.filter_by_role';
